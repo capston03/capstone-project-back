1a06a3e59e26bc5c8222abd87e29e2a2e8813ced
Seongchan Joe <wordnumperiod@gmail.com>
2022-04-11 18:59:01 +0900

    Add system path.

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+import sys
+
 from flask import Flask, render_template, request
 
 from src.handler_db import HandlerDb
 from src.handler_user_db import HandlerUserDb
 from src.model.user import User
+
+sys.path.insert(0, '../src')
 
 app = Flask(__name__)
 
