1f986918cacafe8087e6cbc37862d87eae7ece97
Seongchan Joe <wordnumperiod@gmail.com>
2022-05-26 03:51:42 +0900

    Complete code that makes gltf

---
+++ src/image_processing/triangle_mesh.py
@@ -0,0 +1,29 @@
+import open3d
+import numpy as np
+
+# Example mesh
+# x, y coordinates:
+# [0: (-1, 2)]__________[1: (1, 2)]
+#             \        /\
+#              \  (0) /  \
+#               \    / (1)\
+#                \  /      \
+#      [2: (0, 0)]\/________\[3: (2, 0)]
+#
+# z coordinate: 0
+
+mesh = open3d.geometry.TriangleMesh()
+np_vertices = np.array([[-1, 2, 0],
+                        [1, 2, 0],
+                        [0, 0, 0],
+                        [2, 0, 0]])
+np_triangles = np.array([[0, 2, 1],
+                         [1, 2, 3]]).astype(np.int32)
+mesh.vertices = open3d.utility.Vector3dVector(np_vertices)
+
+# From numpy to Open3D
+mesh.triangles = open3d.utility.Vector3iVector(np_triangles)
+
+# From Open3D to numpy
+np_triangles = np.asarray(mesh.triangles)
+open3d.visualization.draw_geometries([mesh])
